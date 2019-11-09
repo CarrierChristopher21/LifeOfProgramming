83e0b2289e71822dbb24d3636f2715d6bc26c1b7
CarrierChristopher21 <CarrierChristopher21@gmail.com>
2019-11-09 13:38:38 -0500

    Chapter 15 Update

--- Angular/Pro_Angular_6_Projects/Chapter-15-example-Part-5/src/app/app.module.ts
+++ Angular/Pro_Angular_6_Projects/Chapter-15-example-Part-5/src/app/app.module.ts
@@ -1,14 +1,18 @@
 
 
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+//  The Contents of app.module.ts File
 
 //import { AppComponent } from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { ProductComponent } from "./component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { PaAttrDirective } from "./attr.directive";
+import { PaModel } from "./twoway.directive";
+
 
 @NgModule({
-	declarations: [ProductComponent],
+	declarations: [ProductComponent, PaAttrDirective, PaModel],
 	imports: [BrowserModule, FormsModule, ReactiveFormsModule],
 	providers: [],
 	bootstrap: [ProductComponent]
